199b6e6e87a90dc736eab68f85ae2c748bd3a416
codemonkey222 <61676883+codemonkey222@users.noreply.github.com>
2021-06-30 17:57:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function alertBuy() {
-  window.location.href = 'https://google.com';
+  alert("You've just bought on Tom's main frame ");
   // var products = document.getElementsByName("product");
   // var product = "A";
   // for (var i = 0, length = products.length; i < length; i++) {
